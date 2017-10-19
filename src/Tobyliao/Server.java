e1543266e5fba4f7ca922e5ab32cb31d41749665
Tobyliao <Tobyliao@ip136-61.wlan.ntust.edu.tw>
2017-10-19 22:54:51 +0800

    Read and send

--- src/Tobyliao/Server.java
+++ src/Tobyliao/Server.java
@@ -1,33 +1,34 @@
 package Tobyliao;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class Server {
-	public static void main(String args[]) {
-		int ServerPort = 1111;
-		System.out.println("Server Port:"+ServerPort);
-		
-		try {
-			DatagramSocket socket = new DatagramSocket(ServerPort);
-			byte[] msgBuffer = new byte[1024];
-			DatagramPacket packet = new DatagramPacket(msgBuffer,msgBuffer.length);
-			 while (true)
-             {
-                     socket.receive(packet);
-                     String message = new String(msgBuffer, 0, packet.getLength());
-                     System.out.println("UDPServer: Message received = " + message);
-                     packet.setLength(msgBuffer.length);
-             }
-			
-			
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-            System.out.println("Error in getting the Data from UDP Client");
-		}
-		
-	}
+	public static void main(String[] args)throws IOException{  
+        String str_send = "Hello UDPclient \nStdId +2 B10307110";  
+        byte[] buf = new byte[1024];  
+        //服务端在3000端口监听接收到的数据  
+        DatagramSocket ds = new DatagramSocket(1024);  
+        //接收从客户端发送过来的数据  
+        DatagramPacket dp_receive = new DatagramPacket(buf, 1024);  
+        System.out.println("server is on，waiting for client to send data......");  
+        boolean f = true;  
+        while(f){  
+            //服务器端接收来自客户端的数据  
+            ds.receive(dp_receive);  
+            System.out.println("server received data from client：");  
+            String str_receive = new String(dp_receive.getData(),0,dp_receive.getLength()) +   
+                    " from " + dp_receive.getAddress().getHostAddress() + ":" + dp_receive.getPort();  
+            System.out.println(str_receive);  
+            //数据发动到客户端的3000端口  
+            DatagramPacket dp_send= new DatagramPacket(str_send.getBytes(),str_send.length(),dp_receive.getAddress(),9000);  
+            ds.send(dp_send);  
+            //由于dp_receive在接收了数据之后，其内部消息长度值会变为实际接收的消息的字节数，  
+            //所以这里要将dp_receive的内部消息长度重新置为1024  
+            dp_receive.setLength(1024);  
+        }  
+        ds.close();  
+    }
 
 }
